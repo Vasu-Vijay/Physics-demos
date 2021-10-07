5b0ee34cd92d2f04c15efc7c58bb2fc3867ea4c6
Vasu-Vijay <palasusetu@gmail.com>
2021-10-08 01:17:10 +0530

    another commit.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+var isFirst1=true, isFirst2=true;
 $(async function(){
 	if(window.location.href.indexOf("html")!=-1){
 		$("#a1").attr("href","./waves/index.html");
 		$("#a2").attr("href","./gravity/index.html");
 		$("#a3").attr("href","./electrostatics/index.html");
 	}
+	$("#rickroll").hover((e)=>{
+		if(isFirst1){
+			$("#rickroll").html("<a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"><img src=\"https://c.tenor.com/lBK-Zii3--IAAAAM/rickroll.gif\"></a>")
+			console.log('d');
+			isFirst1=false;
+			isFirst2=true;
+		}
+		
+	},(e)=>{
+		if(isFirst2){
+			$("#rickroll").html("")
+			console.log('fd');
+			isFirst2=false;
+			isFirst1=true;
+		}
+	})
 })
